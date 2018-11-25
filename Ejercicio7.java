e830bfe8699e0003fd6c4fbfa7d78cd1ab10814c
manolo <joseignacio.martinez@ikasle.egibide.org>
2018-11-25 23:46:06 +0100

    o

---
+++ Ejercicio7.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg7;
+
+import java.until.Arrays;
+import javax.swing.JOptionPane;
+public class Ejercicio7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int [] codprod = { 10,23,30,47,55,65,135,526,663};
+         int[] cantidad = new int [10];
+         
+        
+         do{
+             int cod= Integer.parseInt(JOptionPane.showInputDialog(null,"codigo del producto"));
+             
+         }
+    }
+    
+}
