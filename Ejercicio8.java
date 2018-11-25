ec06da99666dd5e7fb2a7d9141276e2c89881b19
manolo <joseignacio.martinez@ikasle.egibide.org>
2018-11-25 23:46:55 +0100

    8

---
+++ Ejercicio8.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg8;
+
+import java.until.Arrays;
+import javax.swing.JOptionPane;
+
+public class Ejercicio8 {
+
+  
+    public static void main(String[] args) {
+       int matriz[][] = new int [10][10];
+       int suma[] new int [10];
+       
+       do{
+           int fila = Integer.parseInt(JOptionPane.showInputDialog(null, "introduzca el numero de fila"));
+           int colummna = Integer.parseInt(JOptionPane.showInputDialog(null, "introduzca el numero de colummnas"));
+           int valor = Integer.parseInt(JOptionPane.showInputDialog(null, "introduzca un valor"));
+           
+       }
+    }
+    
+}
